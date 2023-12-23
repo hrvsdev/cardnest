137b6d923a07d8b91e5443a06d35ea98fcabd044
Harsh Vyas <kumarvyasss@gmail.com>
2023-12-23 19:31:03 +0530

    Move Header to diff directory

---
+++ src/pages/Home/Header/index.tsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from "react";
+
+import { IconCircleX, IconSearch } from "@tabler/icons-react";
+
+import { Show } from "@components/Show.tsx";
+
+import { c } from "@utils/styles.ts";
+
+export function Header() {
+	const [value, setValue] = useState("");
+
+	const inputRef = useRef<HTMLInputElement>(null);
+	const showClearIcon = value.length > 0;
+
+	const onClear = () => {
+		setValue("");
+		inputRef.current?.focus();
+	};
+
+	return (
+		<div className="p-4 bg-th-darker-blue">
+			<h1 className="font-semibold text-heading mb-4 pl-0.5">Home</h1>
+			<div className="flex items-center w-full relative">
+				<IconSearch size={24} className="absolute text-th-white left-3 opacity-60" />
+				<Show when={showClearIcon}>
+					<IconCircleX
+						size={24}
+						onClick={onClear}
+						className="absolute text-th-white right-3 opacity-60 peer"
+					/>
+				</Show>
+				<input
+					id="search"
+					ref={inputRef}
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					placeholder="Enter card number, bank or network"
+					className={c(
+						"w-full rounded-2xl pl-12 py-3 text-th-white bg-th-white bg-opacity-5 focus:bg-opacity-10 peer-active:bg-opacity-10",
+						showClearIcon ? "pr-12" : "pr-4"
+					)}
+				/>
+			</div>
+		</div>
+	);
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,12 @@
-import { IconSearch } from "@tabler/icons-react";
-
+import { Header } from "./Header";
 import { TabBar } from "./TabBar";
 
 export function Home() {
 	return (
 		<main className="relative h-full w-full">
-			<div className="pt-4 px-4">
-				<h1 className="font-semibold text-3xl mb-4">Search</h1>
-				<div className="flex items-center w-full relative">
-					<IconSearch size={24} className="absolute text-th-white left-3 opacity-60" />
-					<input
-						className="w-full rounded-2xl h-12 py-3 pl-12 pr-4 text-th-white bg-th-white bg-opacity-10 focus:bg-opacity-5"
-						placeholder="Enter card number, bank or network"
-					/>
-				</div>
-			</div>
+			<Header />
+			<div className="" />
 			<TabBar />
 		</main>
 	);
 }
-
