c9d5f09b22b1b9a4e4c668df51507bb05fb2a373
Harsh Vyas <kumarvyasss@gmail.com>
2023-12-25 19:47:18 +0530

    Add TabBar icons color changing transition

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 import { CreditCardIcon, HomeIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
+import { c } from "@utils/styles.ts";
+
+
+
+
+
 const tabs = [
 	{
 		icon: HomeIcon,
@@ -45,7 +51,13 @@ function TabButton({ icon: I, isActive, onClick }: TabButtonProps) {
 	return (
 		<button onClick={onClick} className="w-1/3">
 			<div className="flex items-center justify-center gap-2.5 rounded-2xl mx-auto">
-				<I className={isActive ? "text-th-sky" : "text-th-white/70"} width={24} />
+				<I
+					width={24}
+					className={c(
+						"transition-colors duration-300",
+						isActive ? "text-th-sky" : "text-th-white/70"
+					)}
+				/>
 			</div>
 		</button>
 	);
