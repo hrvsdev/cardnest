48c9f70205ddbd20c78bc0f2927a69a6a4e7efad
Harsh Vyas <kumarvyasss@gmail.com>
2023-12-25 14:44:44 +0530

    Add basic config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths()],
+	plugins: [
+		react(),
+		viteTsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true
+			}
+		})
+	],
 	server: {
 		port: 4000
 	}
