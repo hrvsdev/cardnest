216044de977d76f29d7488c5568e042d6c143e7d
Harsh Vyas <kumarvyasss@gmail.com>
2023-12-25 14:59:04 +0530

    Add pwa files

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,28 @@ export default defineConfig({
 		viteTsconfigPaths(),
 		VitePWA({
 			registerType: "autoUpdate",
-			devOptions: {
-				enabled: true
+			includeAssets: [
+				"favicon.png",
+				"robots.txt",
+				"apple-touch-icon.png",
+				"icons/*.svg",
+				"fonts/*.woff2"
+			],
+			manifest: {
+				theme_color: "#00060c",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any maskable"
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png"
+					}
+				]
 			}
 		})
 	],
