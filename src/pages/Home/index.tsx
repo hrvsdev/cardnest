7ef9624ef6c117442252b3cdda6d9843bcbdcbce
Harsh Vyas <kumarvyasss@gmail.com>
2023-12-31 20:09:25 +0530

    Add initial Card UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,10 @@ import { Header } from "@components/Header";
 
 import { c } from "@utils/styles.ts";
 
+
+
+
+
 export function Home() {
 	return (
 		<Fragment>
@@ -18,24 +22,6 @@ export function Home() {
 	);
 }
 
-const cardColorClassName = {
-	sky: "from-sky-500 to-sky-700",
-	pink: "from-pink-500 to-pink-700",
-	red: "from-red-500 to-red-700",
-	cyan: "from-cyan-500 to-cyan-700",
-	yellow: "from-yellow-500 to-yellow-700",
-	blue: "from-blue-500 to-blue-700",
-	green: "from-green-500 to-green-700",
-	emerald: "from-emerald-500 to-emerald-700",
-	fuchsia: "from-fuchsia-500 to-fuchsia-700",
-	purple: "from-purple-500 to-purple-700",
-	violet: "from-violet-500 to-violet-700",
-	indigo: "from-indigo-500 to-indigo-700",
-	orange: "from-orange-500 to-orange-700",
-	teal: "from-teal-500 to-teal-700",
-	rose: "from-rose-500 to-rose-700"
-};
-
 type CardColor = keyof typeof cardColorClassName;
 
 type Props = {
@@ -44,11 +30,42 @@ type Props = {
 
 function Card({ color }: Props) {
 	const cl = cardColorClassName[color];
+
+	const formattedCardNumber = card.number.replace(/(.{4})/g, "$1 ");
+
 	return (
 		<div className={c("w-full aspect-payment-card bg-gradient-to-br rounded-2xl", cl)}>
-			<div className="flex items-center justify-center h-full tracking-wider">
-				{color.toUpperCase()}
+			<div className="flex flex-col justify-between w-full h-full p-6">
+				<div className="flex justify-between">
+					<div>
+						<p className="tracking-widest text-[10px] font-light opacity-80">CARDHOLDER</p>
+						<p className="tracking-wider text-lg">{card.cardholder}</p>
+					</div>
+				</div>
+				<div className="tracking-widest text-xl mt-8">{formattedCardNumber}</div>
+				<div className="flex justify-between">
+					<div>
+						<p className="tracking-widest text-[10px] font-light opacity-80">EXPIRES</p>
+						<p className="tracking-wider">
+							{card.expiry.month}/{card.expiry.year}
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 }
+
+const cardColorClassName = {
+	sky: "from-sky-500 to-sky-700"
+};
+
+const card = {
+	number: "4641060453063779",
+	expiry: {
+		month: 10,
+		year: 2028
+	},
+	cardholder: "John JM Marston",
+	network: "visa"
+};
