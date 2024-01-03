a33d6a79ea3c4870d84841c819ec438496de6b67
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-03 22:23:03 +0530

    Add initial `AddCard` page UI

--- src/pages/AddCard/index.tsx
+++ src/pages/AddCard/index.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+
 import { PageContainer } from "@components/Containers";
 import { HeaderTitle } from "@components/Header/HeaderTitle.tsx";
 
@@ -7,7 +9,20 @@ export function AddCard() {
 	return (
 		<Fragment>
 			<HeaderTitle title="Add Card" />
-			<PageContainer />
+			<PageContainer className="flex flex-col justify-center items-center">
+				<div className="my-auto">
+					<PlusCircleIcon className="size-28 text-gray-400 mb-4 mx-auto" strokeWidth={1} />
+					<p className="text-center text-th-white/70">
+						Add a new card to add to your collection. <br />
+						On our website, you can't scan cards yet but you can add them manually.
+					</p>
+				</div>
+				<button className="bg-th-sky w-full h-12.5 text-md rounded-xl mt-auto">Add Card</button>
+			</PageContainer>
 		</Fragment>
 	);
 }
+
+//<button className="bg-th-sky w-full h-12.5 text-md rounded-xl mt-auto">
+// 					Add Card
+// 				</button>
