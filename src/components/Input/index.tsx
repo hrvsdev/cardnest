a2371e5bdc67de98bad636323c609a8da6309d88
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-10 19:00:39 +0530

    Add `Input` component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, FocusEvent, HTMLInputTypeAttribute } from "react";
+
+import { Show } from "@components/Show";
+
+import { c } from "@utils/styles.ts";
+
+type Props = {
+	value?: string | number;
+	defaultValue?: string | number;
+	name?: string;
+	type?: HTMLInputTypeAttribute;
+	id?: string;
+	label?: string;
+	placeholder?: string;
+	maxLength?: number;
+	minLength?: number;
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+	onFocus?: (e: FocusEvent<HTMLInputElement>) => void;
+	onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
+	className?: string;
+};
+
+export function Input(props: Props) {
+	return (
+		<div className="space-y-2">
+			<Show when={props.label}>
+				<label className="text-th-white/80 pl-2" htmlFor={props.id}>
+					Cardholder
+				</label>
+			</Show>
+			<input
+				type={props.type}
+				name={props.name}
+				id={props.id}
+				minLength={props.minLength}
+				maxLength={props.maxLength}
+				value={props.value}
+				defaultValue={props.defaultValue}
+				onChange={props.onChange}
+				onFocus={props.onFocus}
+				onBlur={props.onBlur}
+				placeholder={props.placeholder}
+				className={c(
+					"w-full rounded-2xl px-4 py-3 tracking-widest placeholder:tracking-normal text-th-white bg-th-white bg-opacity-5 focus:bg-opacity-10",
+					props.className
+				)}
+			/>
+		</div>
+	);
+}
