d341d641c40a70289e366e5c6c9e09799e9f5063
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-10 11:35:48 +0530

    Add focused state to card editor on input

--- src/pages/AddCard/Editor/index.tsx
+++ src/pages/AddCard/Editor/index.tsx
@@ -6,12 +6,14 @@ import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { Card } from "@components/Card";
 import { PageContainer } from "@components/Containers";
 
-import { CardDetails } from "@t/card.ts";
+import { CardDetails, CardElement } from "@t/card.ts";
 
 export function AddCardEditor() {
 	const [cardNumber, setCardNumber] = useState("");
 	const [expiry, setExpiry] = useState("");
 	const [cardholder, setCardholder] = useState("");
+
+	const [focused, setFocused] = useState<CardElement>();
 
 	const onCardInput = (e: ChangeEvent<HTMLInputElement>) => {
 		const filteredValue = e.target.value.replace(/\D/g, "");
@@ -48,43 +50,58 @@ export function AddCardEditor() {
 		<Fragment>
 			<SubPageHeader title="New Card" />
 			<PageContainer>
-				<Card color="sky" card={CARD_DETAILS} usePlaceholders />
+				<Card color="sky" card={CARD_DETAILS} usePlaceholders focused={focused} />
 				<div className="space-y-6 mt-8">
-					<label className="block space-y-2">
-						<p className="text-th-white/80 pl-2">Card number</p>
+					<div className="space-y-2">
+						<label className="text-th-white/80 pl-2" htmlFor="number">
+							Card network
+						</label>
 						<input
 							type="text"
+							id="number"
 							maxLength={19}
 							inputMode="numeric"
 							value={cardNumber}
 							onChange={onCardInput}
+							onFocus={() => setFocused("number")}
+							onBlur={() => setFocused(undefined)}
 							className="w-full rounded-2xl px-4 py-3 tracking-widest placeholder:tracking-normal text-th-white bg-th-white bg-opacity-5 focus:bg-opacity-10"
 							placeholder="Enter card number"
 						/>
-					</label>
-					<label className="block space-y-2">
-						<p className="text-th-white/80 pl-2">Expiry date</p>
+					</div>
+					<div className="space-y-2">
+						<label className="text-th-white/80 pl-2" htmlFor="expiry">
+							Expiry date
+						</label>
 						<input
 							type="text"
+							id="expiry"
 							maxLength={5}
 							inputMode="numeric"
 							value={expiry}
 							onChange={onExpiryInput}
+							onFocus={() => setFocused("expiry")}
+							onBlur={() => setFocused(undefined)}
 							className="w-full rounded-2xl px-4 py-3 tracking-widest placeholder:tracking-normal text-th-white bg-th-white bg-opacity-5 focus:bg-opacity-10"
 							placeholder="Enter card expiry date"
 						/>
-					</label>
-					<label className="block space-y-2">
-						<p className="text-th-white/80 pl-2">Cardholder</p>
+					</div>
+					<div className="space-y-2">
+						<label className="text-th-white/80 pl-2" htmlFor="cardholder">
+							Cardholder
+						</label>
 						<input
 							type="text"
+							id="cardholder"
 							maxLength={30}
 							value={cardholder}
 							onChange={onCardholderInput}
+							onFocus={() => setFocused("cardholder")}
+							onBlur={() => setFocused(undefined)}
 							className="w-full rounded-2xl px-4 py-3 tracking-widest placeholder:tracking-normal text-th-white bg-th-white bg-opacity-5 focus:bg-opacity-10"
 							placeholder="Enter cardholder name"
 						/>
-					</label>
+					</div>
 				</div>
 			</PageContainer>
 		</Fragment>
