3498bd320cbaa9798f0844753358edb822bad0ee
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-14 13:18:58 +0530

    Refactor card data state hooks

--- src/hooks/cards.ts
+++ src/hooks/cards.ts
@@ -1,16 +1,38 @@
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { atom, useAtomValue, useSetAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-import { CardDetails, CardTheme } from "@t/card.ts";
+import { genId } from "@utils/id.ts";
 
-type Meta = {
-	theme: CardTheme;
+import { CardData, CardFullProfile } from "@t/card.ts";
+
+const cardsAtom = atomWithStorage<Record<string, CardData>>("cardnest/cards", {});
+
+const getAllCardsAtom = atom((get) => {
+	return Object.values(get(cardsAtom));
+});
+
+const addCardAtom = atom(null, (_, set, card: CardFullProfile) => {
+	const id = genId();
+	const newCard = { id, data: card };
+	set(cardsAtom, (cards) => ({ ...cards, [id]: newCard }));
+});
+
+const updateCardAtom = atom(null, (_, set, cardData: CardData) => {
+	set(cardsAtom, (cards) => ({ ...cards, [cardData.id]: cardData }));
+});
+
+const deleteCardAtom = atom(null, (_, set, id: string) => {
+	set(cardsAtom, (cards) => {
+		delete cards[id];
+		return { ...cards };
+	});
+});
+
+export const useAllCards = () => useAtomValue(getAllCardsAtom);
+export const useCard = (id: string) => {
+	return useAtomValue(cardsAtom)[id];
 };
 
-type Cards = Array<CardDetails & Meta>;
-
-const cardsAtom = atomWithStorage<Cards>("cardnest/cards", []);
-
-export const useCards = () => useAtom(cardsAtom);
-export const useCardsValue = () => useAtomValue(cardsAtom);
-export const useSetCards = () => useSetAtom(cardsAtom);
+export const useAddCard = () => useSetAtom(addCardAtom);
+export const useUpdateCard = () => useSetAtom(updateCardAtom);
+export const useDeleteCard = () => useSetAtom(deleteCardAtom);
