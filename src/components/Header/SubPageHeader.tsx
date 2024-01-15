7a680de0f3da17e95762790e424a6a40b9b4443a
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-15 13:18:56 +0530

    Add optional right button on `SubPageHeader`

--- src/components/Header/SubPageHeader.tsx
+++ src/components/Header/SubPageHeader.tsx
@@ -1,16 +1,25 @@
+import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
-import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { IconChevronLeft } from "@tabler/icons-react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 import { backgroundRange, borderOpacityRange } from "@components/Header/shared";
+import { Show } from "@components/Show";
 
 const HEADER_TOP_OFFSET = 0;
 const HEADER_BOTTOM_OFFSET = 16;
 
 const inputRange = [HEADER_TOP_OFFSET, HEADER_BOTTOM_OFFSET];
 
-export function SubPageHeader({ title }: { title: string }) {
+type Props = {
+	title: string;
+	rightButtonLabel?: string;
+	rightButtonIcon?: ReactNode;
+	onRightButtonClick?: () => void;
+};
+
+export function SubPageHeader(props: Props) {
 	const { scrollY } = useScroll();
 
 	const borderOpacity = useTransform(scrollY, inputRange, borderOpacityRange);
@@ -20,10 +29,19 @@ export function SubPageHeader({ title }: { title: string }) {
 		<motion.div style={{ background }} className="sticky top-0 z-10 backdrop-blur-md">
 			<div className="flex items-center justify-center relative w-full h-12">
 				<Link to=".." className="flex items-center gap-1 absolute left-0 h-full px-4 text-th-sky">
-					<ChevronLeftIcon strokeWidth={2.5} className="size-4" />
+					<IconChevronLeft strokeWidth={2.5} size={20} />
 					<span className="text-sm">Back</span>
 				</Link>
-				<div>{title}</div>
+				<div>{props.title}</div>
+				<Show when={props.rightButtonLabel || props.rightButtonIcon}>
+					<button
+						className="flex items-center gap-1 absolute right-0 h-full px-4 text-th-sky"
+						onClick={props.onRightButtonClick}
+					>
+						{props.rightButtonIcon}
+						<span className="text-sm">{props.rightButtonLabel}</span>
+					</button>
+				</Show>
 			</div>
 			<motion.div style={{ height: 1, opacity: borderOpacity }} className="bg-th-white" />
 		</motion.div>
