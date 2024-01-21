fa178262bd4a9354efa745bb4a0ae2834063ee00
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-21 14:45:59 +0530

    Add `Button` themes and variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,39 @@
 import { c } from "@utils/styles.ts";
 
-type ButtonVariant = "primary" | "danger";
+type Theme = "primary" | "danger";
+type Variant = "solid" | "flat";
 
 type Props = {
 	label: string;
 	type?: "button" | "submit" | "reset";
-	variant?: ButtonVariant;
+	theme?: Theme;
+	variant?: Variant;
 	onClick?: () => void;
 	className?: string;
 };
 
-const variants: Record<ButtonVariant, string> = {
-	primary: "bg-th-sky shadow-th-sky/15",
-	danger: "bg-th-red shadow-th-red/25",
+const styles: Record<Variant, Record<Theme, string>> = {
+	solid: {
+		primary: "bg-th-sky shadow-th-sky/15 text-th-white shadow-lg",
+		danger: "bg-th-red shadow-th-red/25 text-th-white shadow-lg"
+	},
+	flat: {
+		primary: "text-th-sky bg-th-sky/10",
+		danger: "text-th-red bg-th-red/10"
+	}
 };
 
-export function Button({ label, type, onClick, className, variant = "primary" }: Props) {
+export function Button(props: Props) {
+	const { label, type, onClick, className, theme = "primary", variant = "solid" } = props;
 	return (
 		<button
 			type={type}
 			onClick={onClick}
-			className={c("w-full h-12 text-md rounded-2xl shadow-lg transition-all dur hover:brightness-110 active:scale-98", variants[variant], className)}
+			className={c(
+				"w-full h-12 text-md rounded-2xl transition-all duration-200 hover:brightness-110 active:scale-98",
+				styles[variant][theme],
+				className
+			)}
 		>
 			{label}
 		</button>
