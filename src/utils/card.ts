07763f32f8b6cf29c1fd6b22ba6dd976026ab52c
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-22 12:57:03 +0530

    Add util function to fetch card network

--- src/utils/card.ts
+++ src/utils/card.ts
@@ -1,4 +1,6 @@
-import { CardTheme } from "@t/card";
+import ky from "ky";
+
+import { CardTheme, PaymentNetwork } from "@t/card";
 
 export const cardThemeStyles: Record<CardTheme, string> = {
 	sky: "from-sky-500 to-sky-700",
@@ -26,3 +28,34 @@ export const getRandomCardTheme = () => {
 
 export const addSpaces = (str: string) => str.replace(/(.{4})/g, "$1 ").trim();
 export const removeSpaces = (str: string) => str.replace(/\s/g, "");
+
+export const getApproxCardNetwork = async (cardNumber: string) => {
+	let cardNetwork: PaymentNetwork = "other";
+
+	const url = `https://data.handyapi.com/bin/${cardNumber.slice(0, 6)}`;
+
+	try {
+		const res: { Scheme: String } = await ky(url).json();
+		const scheme = res.Scheme.toLowerCase();
+
+		for (const [key, value] of Object.entries(cardMap)) {
+			if (value.some((v) => scheme.includes(v))) {
+				cardNetwork = key as PaymentNetwork;
+				break;
+			}
+		}
+	} catch (e) {
+		console.log(e);
+	}
+
+	return cardNetwork;
+};
+
+export const cardMap: Partial<Record<PaymentNetwork, Array<string>>> = {
+	visa: ["visa"],
+	amex: ["amex", "american"],
+	diners: ["diners"],
+	discover: ["discover"],
+	mastercard: ["master", "maestro"],
+	rupay: ["rupay"]
+};
