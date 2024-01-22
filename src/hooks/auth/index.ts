abfd79a135d09fc9e3620f044b1fff7c479db0fa
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-22 18:06:07 +0530

    Add initial key state

--- src/hooks/auth/index.ts
+++ src/hooks/auth/index.ts
@@ -1,6 +1,13 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-const isAuthenticatedAtom = atom(process.env.NODE_ENV === "development");
+import { getFromLocalStorage } from "@utils/local-storage";
 
+const KEY = "cardnest/key";
+
+const keyAtom = atomWithStorage<string | null>(KEY, getFromLocalStorage(KEY));
+
+const isAuthenticatedAtom = atom((get) => Boolean(get(keyAtom)));
+
+export const UseSetKey = () => useSetAtom(keyAtom);
 export const UseIsAuthenticatedValue = () => useAtomValue(isAuthenticatedAtom);
-export const UseSetIsAuthenticated = () => useSetAtom(isAuthenticatedAtom);
