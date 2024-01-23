ddfd39060df54fbb156e8690be31d9ac5f0113db
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-23 19:40:36 +0530

    Add hooks to check if user has created a pin

--- src/hooks/auth/index.ts
+++ src/hooks/auth/index.ts
@@ -1,8 +1,16 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+const KEY = "cardnest/hasCreatedPin";
+
+export const hasCreatedPin = atomWithStorage(KEY, false);
 
 export const pinAtom = atom<string | null>(null);
 
 export const isAuthenticatedAtom = atom((get) => Boolean(get(pinAtom)));
+
+export const useHasCreatedPinValue = () => useAtomValue(hasCreatedPin);
+export const setHasCreatedPin = () => useSetAtom(hasCreatedPin);
 
 export const UseSetKey = () => useSetAtom(pinAtom);
 export const UseIsAuthenticatedValue = () => useAtomValue(isAuthenticatedAtom);
