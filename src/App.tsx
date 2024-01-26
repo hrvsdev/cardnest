1828c52ac0e5b0d5586f9c90ba8cc5f744a78070
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-26 23:00:40 +0530

    Add logic to conditionally show routes based on auth

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,24 @@ import { ConfirmPin } from "@pages/Pin/ConfirmPin";
 import { CreatePin } from "@pages/Pin/CreatePin";
 import { User } from "@pages/User";
 
-import { useIsAuthenticatedValue } from "@hooks/auth";
+import { useHasCreatedPin, useIsAuthenticatedValue } from "@hooks/auth";
 
 export default function App() {
-	// @ts-ignore
 	const isAuthenticated = useIsAuthenticatedValue();
+	const hasCreatedPin = useHasCreatedPin();
+
+	const showApp = hasCreatedPin ? isAuthenticated : true;
+
 	return (
 		<Suspense>
 			<main className="flex flex-col min-h-dvh h-full w-full">
-				<Routes>
-					<Route path="home/*" element={<Home />} />
-					<Route path="add/*" element={<AddCard />} />
-					<Route path="user/*" element={<User />} />
-					<Route path="pin/create">
-						<Route index element={<CreatePin />} />
-						<Route path="confirm" element={<ConfirmPin />} />
-					</Route>
-					<Route path="*" element={<Navigate to="home" />} />
-				</Routes>
+				<CommonRoutes />
+				{showApp ? <AuthenticatedRoutes /> : <UnauthenticatedRoutes />}
 			</main>
 		</Suspense>
 	);
 }
 
-// @ts-ignore
 function AuthenticatedRoutes() {
 	return (
 		<Routes>
@@ -43,16 +37,22 @@ function AuthenticatedRoutes() {
 	);
 }
 
-// @ts-ignore
 function UnauthenticatedRoutes() {
 	return (
 		<Routes>
 			<Route index element={<Pin />} />
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
+	);
+}
+
+function CommonRoutes() {
+	return (
+		<Routes>
 			<Route path="pin/create">
 				<Route index element={<CreatePin />} />
 				<Route path="confirm" element={<ConfirmPin />} />
 			</Route>
-			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	);
 }
