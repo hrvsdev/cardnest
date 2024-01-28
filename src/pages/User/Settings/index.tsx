d5a55bd9c1b999710dd0d479639beb926f9f4580
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-28 18:08:18 +0530

    Add links and clicks to settings buttons

--- src/pages/User/Settings/index.tsx
+++ src/pages/User/Settings/index.tsx
@@ -1,21 +1,11 @@
-import { ReactElement, ReactNode } from "react";
-
-import { IconChevronRight, TablerIconsProps } from "@tabler/icons-react";
+import { ReactNode } from "react";
 
 import { Show } from "@components/Show";
-
-import { c } from "@utils/styles.ts";
 
 type SettingsGroupProp = {
 	title?: string;
 	description?: string;
 	children: ReactNode;
-};
-
-type SettingsButtonProps = {
-	Icon: (props: TablerIconsProps) => ReactElement;
-	title: string;
-	isDanger?: boolean;
 };
 
 export function SettingsGroup({ title, description, children }: SettingsGroupProp) {
@@ -28,24 +18,6 @@ export function SettingsGroup({ title, description, children }: SettingsGroupPro
 			<Show when={description}>
 				<p className="pl-2 text-sm mt-2 text-th-white/60">{description}</p>
 			</Show>
-		</div>
-	);
-}
-
-export function SettingsButton({ Icon, title, isDanger }: SettingsButtonProps) {
-	return (
-		<div
-			className={c(
-				"flex items-center px-3 py-2.5 rounded-sm first:rounded-t-xl last:rounded-b-xl",
-				isDanger ? "bg-th-red/10 text-th-red" : "bg-th-white/5"
-			)}
-		>
-			<Icon size={20} className="mr-2" />
-			<p>{title}</p>
-			<IconChevronRight
-				size={20}
-				className={c("ml-auto", isDanger ? "text-th-red/50" : "text-th-white/40")}
-			/>
 		</div>
 	);
 }
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,32 +1,50 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import { IconMoonStars, IconPasswordFingerprint, IconTrash } from "@tabler/icons-react";
 
-import { SettingsButton, SettingsGroup } from "@pages/User/Settings";
+import { DeleteDataDialog } from "@pages/User/DeleteDataDialog";
+import { SettingsGroup } from "@pages/User/Settings";
+import { SettingsButton, SettingsLink } from "@pages/User/Settings/Button.tsx";
 
 import { PageContainer } from "@components/Containers";
 import { HeaderTitle } from "@components/Header/HeaderTitle.tsx";
 import { TabBar } from "@components/TabBar";
 
 export function User() {
+	const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+	const deleteData = () => {
+		console.log("Deleting data...");
+	};
+
 	return (
 		<Fragment>
 			<HeaderTitle title="You" />
 
 			<PageContainer className="space-y-6">
 				<SettingsGroup title="App settings">
-					<SettingsButton Icon={IconPasswordFingerprint} title="Security" />
-					<SettingsButton Icon={IconMoonStars} title="Appearance" />
+					<SettingsLink Icon={IconPasswordFingerprint} title="Security" to="security" />
+					<SettingsLink Icon={IconMoonStars} title="Appearance" to="appearance" />
 				</SettingsGroup>
-				<SettingsGroup
-					title="Danger zone"
-					description="Deleting data will forever delete all data from all your devices and there is no way to recover it."
-				>
-					<SettingsButton Icon={IconTrash} title="Delete all data" isDanger />
+				<SettingsGroup title="Danger zone" description={DELETE_DESCRIPTION}>
+					<SettingsButton
+						isDanger
+						Icon={IconTrash}
+						title="Delete all data"
+						onClick={() => setShowDeleteDialog(true)}
+					/>
 				</SettingsGroup>
 			</PageContainer>
-
 			<TabBar />
+
+			<DeleteDataDialog
+				show={showDeleteDialog}
+				onConfirm={deleteData}
+				onClose={() => setShowDeleteDialog(false)}
+			/>
 		</Fragment>
 	);
 }
+
+const DELETE_DESCRIPTION =
+	"Deleting data will forever delete all data from all your devices and there is no way to recover it.";
