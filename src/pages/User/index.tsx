17689d65d91f423a6c3963292cca942fc97c8628
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-29 23:21:58 +0530

    Add `Security` and `UserInterface` pages to settings

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,16 +1,29 @@
 import { Fragment, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import { IconMoonStars, IconPasswordFingerprint, IconTrash } from "@tabler/icons-react";
+import { DeleteDataDialog } from "pages/User/components/DeleteDataDialog";
+import { SettingsGroup } from "pages/User/components/Settings";
 
-import { DeleteDataDialog } from "@pages/User/DeleteDataDialog";
-import { SettingsGroup } from "@pages/User/Settings";
-import { SettingsButton, SettingsLink } from "@pages/User/Settings/Button.tsx";
+import { SettingsButton, SettingsLink } from "@pages/User/components/Settings/Button.tsx";
+import { Security } from "@pages/User/Security";
+import { UserInterface } from "@pages/User/UserInterface";
 
 import { PageContainer } from "@components/Containers";
 import { HeaderTitle } from "@components/Header/HeaderTitle.tsx";
 import { TabBar } from "@components/TabBar";
 
 export function User() {
+	return (
+		<Routes>
+			<Route index element={<UserPage />} />
+			<Route path="security" element={<Security />} />
+			<Route path="interface" element={<UserInterface />} />
+		</Routes>
+	);
+}
+
+function UserPage() {
 	const [showDeleteDialog, setShowDeleteDialog] = useState(false);
 
 	const deleteData = () => {
@@ -24,7 +37,7 @@ export function User() {
 			<PageContainer className="space-y-6">
 				<SettingsGroup title="App settings">
 					<SettingsLink Icon={IconPasswordFingerprint} title="Security" to="security" />
-					<SettingsLink Icon={IconMoonStars} title="Appearance" to="appearance" />
+					<SettingsLink Icon={IconMoonStars} title="User Interface" to="interface" />
 				</SettingsGroup>
 				<SettingsGroup title="Danger zone" description={DELETE_DESCRIPTION}>
 					<SettingsButton
