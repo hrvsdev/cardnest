be5d2f44abd848b9c9a8d0888087fd36a361b395
Harsh Vyas <kumarvyasss@gmail.com>
2024-01-30 16:41:52 +0530

    Add `Security` page buttons

--- src/pages/User/Security/index.tsx
+++ src/pages/User/Security/index.tsx
@@ -1,5 +1,10 @@
 import { Fragment } from "react";
 
+import { IconLockOpenOff, IconPasswordMobilePhone } from "@tabler/icons-react";
+
+import { SettingsGroup } from "@pages/User/components/Settings";
+import { SettingsButton, SettingsLink } from "@pages/User/components/Settings/Button.tsx";
+
 import { PageContainer } from "@components/Containers";
 import { SubPageHeader } from "@components/Header/SubPageHeader.tsx";
 
@@ -7,7 +12,26 @@ export function Security() {
 	return (
 		<Fragment>
 			<SubPageHeader title="Security" leftIconLabel="Settings" />
-			<PageContainer />
+			<PageContainer className="space-y-6">
+				<SettingsGroup title="Password">
+					<SettingsButton
+						onClick={() => {}}
+						Icon={IconPasswordMobilePhone}
+						title="Change password"
+					/>
+				</SettingsGroup>
+				<SettingsGroup title="Danger zone" description={DESC}>
+					<SettingsLink
+						isDanger
+						to="#"
+						Icon={IconLockOpenOff}
+						title="Remove app password"
+					/>
+				</SettingsGroup>
+			</PageContainer>
 		</Fragment>
 	);
 }
+
+const DESC =
+	"Removing your app password will make all your data accessible to anyone who has access to your device.";
