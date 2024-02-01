aed8b251fe5b4b90cadab5ec9025affa39e4173c
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-01 18:28:31 +0530

    Show `RemovePinDialog` component confirmation before removing pin

--- src/pages/User/Security/index.tsx
+++ src/pages/User/Security/index.tsx
@@ -1,14 +1,18 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { IconLockOpenOff, IconPasswordMobilePhone } from "@tabler/icons-react";
 
 import { CreatePin } from "@pages/CreatePin";
+import { RemovePinDialog } from "@pages/User/components/RemovePinDialog";
 import { SettingsGroup } from "@pages/User/components/Settings";
-import { SettingsLink } from "@pages/User/components/Settings/Button.tsx";
+import { SettingsButton, SettingsLink } from "@pages/User/components/Settings/Button.tsx";
 
 import { PageContainer } from "@components/Containers";
 import { SubPageHeader } from "@components/Header/SubPageHeader.tsx";
+
+import { useRemovePin } from "@hooks/auth";
+import { useRemoveCardsPin } from "@hooks/card/data.ts";
 
 export function Security() {
 	return (
@@ -20,6 +24,15 @@ export function Security() {
 }
 
 function SecurityPage() {
+	const removeCardsPin = useRemoveCardsPin();
+	const removePin = useRemovePin();
+
+	const [showRemovePasswordDialog, setShowRemovePasswordDialog] = useState(false);
+
+	const removeAppPassword = () => {
+		removeCardsPin().then(removePin);
+	};
+
 	return (
 		<Fragment>
 			<SubPageHeader title="Security" leftIconLabel="Settings" />
@@ -28,9 +41,20 @@ function SecurityPage() {
 					<SettingsLink to="pin/create" Icon={IconPasswordMobilePhone} title="Change password" />
 				</SettingsGroup>
 				<SettingsGroup title="Danger zone" description={DESC}>
-					<SettingsLink isDanger to="#" Icon={IconLockOpenOff} title="Remove app password" />
+					<SettingsButton
+						onClick={() => setShowRemovePasswordDialog(true)}
+						Icon={IconLockOpenOff}
+						title="Remove app password"
+						isDanger
+					/>
 				</SettingsGroup>
 			</PageContainer>
+
+			<RemovePinDialog
+				show={showRemovePasswordDialog}
+				onConfirm={removeAppPassword}
+				onClose={() => setShowRemovePasswordDialog(false)}
+			/>
 		</Fragment>
 	);
 }
