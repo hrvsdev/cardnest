dba6586d221f11909196394a68158c70830c8d2a
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-02 21:40:22 +0530

    Add settings to change mask card number

--- src/pages/User/UserInterface/index.tsx
+++ src/pages/User/UserInterface/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 
-import { IconCopy } from "@tabler/icons-react";
+import { IconPassword } from "@tabler/icons-react";
 
 import { SettingsGroup } from "@pages/User/components/Settings";
 import { SettingsToggleButton } from "@pages/User/components/Settings/ToggleButton.tsx";
@@ -8,20 +8,26 @@ import { SettingsToggleButton } from "@pages/User/components/Settings/ToggleButt
 import { PageContainer } from "@components/Containers";
 import { SubPageHeader } from "@components/Header/SubPageHeader.tsx";
 
+import { useMaskCardNumber } from "@hooks/preferences";
+
 export function UserInterface() {
+	const [maskCard, setMaskCard] = useMaskCardNumber();
+
 	return (
 		<Fragment>
 			<SubPageHeader title="User Interface" leftIconLabel="Settings" />
 			<PageContainer className="space-y-6">
-				<SettingsGroup>
+				<SettingsGroup title="Card" description={MASK_CARD_DESC}>
 					<SettingsToggleButton
-						title="Copy on Card Preview"
-						Icon={IconCopy}
-						checked={false}
-						onChange={() => {}}
+						title="Mask card number"
+						Icon={IconPassword}
+						checked={maskCard}
+						onChange={setMaskCard}
 					/>
 				</SettingsGroup>
 			</PageContainer>
 		</Fragment>
 	);
 }
+
+const MASK_CARD_DESC = "Mask the card number on the card preview on home screen page.";
