41ac42c979942c1b7148d1f005a3fb3696ba1cc5
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-02 16:55:19 +0530

    Add fixed height to `SettingsButton`

--- src/pages/User/components/Settings/Button.tsx
+++ src/pages/User/components/Settings/Button.tsx
@@ -46,7 +46,7 @@ export function SettingsLink({ to, ...props }: SettingsLinkProps) {
 
 function Content({ title, Icon, isDanger }: ContentProps) {
 	return (
-		<div className="flex items-center justify-between px-3 py-2.5">
+		<div className="flex items-center justify-between w-full h-11 px-3">
 			<span className="flex items-center gap-2">
 				<Icon size={20} />
 				<p>{title}</p>
