c4125af8982254481cc12f3d730d57d655fc4ee3
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-03 20:39:21 +0530

    Add `afterPinVerified` action to actions

--- src/hooks/actions/index.ts
+++ src/hooks/actions/index.ts
@@ -1,16 +1,21 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
 import { focusAtom } from "jotai-optics";
 
+type Action = () => void;
 type AsyncAction = () => Promise<void>;
 
 type Actions = {
 	afterPinCreated: AsyncAction | null;
+	afterPinVerified: Action | null;
 };
 
-const actionsAtom = atom<Actions>({ afterPinCreated: null });
+const actionsAtom = atom<Actions>({ afterPinCreated: null, afterPinVerified: null });
 
 const afterPinCreatedAtom = focusAtom(actionsAtom, (o) => o.prop("afterPinCreated"));
 const afterPinVerifiedAtom = focusAtom(actionsAtom, (o) => o.prop("afterPinVerified"));
 
 export const useAfterPinCreated = () => useAtomValue(afterPinCreatedAtom);
 export const useSetAfterPinCreated = () => useSetAtom(afterPinCreatedAtom);
+
+export const useAfterPinVerified = () => useAtomValue(afterPinVerifiedAtom);
+export const useSetAfterPinVerified = () => useSetAtom(afterPinVerifiedAtom);
