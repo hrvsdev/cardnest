0fbb60cdac3e4056652b8feb4d34cf9b43e7e348
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-06 15:42:19 +0530

    NATIVE | Add card network logos components

---
+++ apps/native/src/components/Logos/MasterCard.tsx
@@ -0,0 +1,18 @@
+import Svg, { Circle, G } from "react-native-svg";
+
+import { themeColors } from "../../styles/colors.ts";
+
+export function MasterCard({ width = 40 }: { width?: number }) {
+	const aspectRatio = 100 / 62.5;
+
+	return (
+		<Svg style={{ width, aspectRatio, overflow: "visible" }} viewBox="0 0 32 20">
+			<G>
+				<G>
+					<Circle cx="10" cy="10" r="10" fill={themeColors.white["85"]} />
+					<Circle cx="22" cy="10" r="10" fill={themeColors.white["65"]} />
+				</G>
+			</G>
+		</Svg>
+	);
+}
---
+++ apps/native/src/components/Logos/Other.tsx
@@ -0,0 +1,5 @@
+import { IconForbid2 } from "@tabler/icons-react";
+
+export function Other({ width = "2rem" }: { width?: number | string }) {
+	return <IconForbid2 width={width} height={width} className="text-th-white/85" />;
+}
