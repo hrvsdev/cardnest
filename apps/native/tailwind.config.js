0924885a3c28ff59a91b40b9d3a243e283d414e9
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-06 08:39:47 +0530

    NATIVE | Add `expo-linear-gradient` library

--- apps/native/tailwind.config.js
+++ apps/native/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"th-black": "#00060c",
+				"th-darker-blue": "#001528",
+				"th-dark-blue": "#00203F",
+				"th-blue": "#003a72",
+				"th-sky": "#3fa1ff",
+				"th-green": "#ADEFD1",
+				"th-red": "#f31260",
+				"th-white": "#F4F7FB"
+			}
+		}
 	},
 	plugins: []
 };
