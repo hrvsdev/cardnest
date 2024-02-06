58eee3ee7fb68babf4c8754d4f97f212c06cb149
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-06 08:39:21 +0530

    NATIVE | Add `colors`

---
+++ apps/native/src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+	thBlack: "#00060c",
+	thDarkerBlue: "#001528",
+	thDarkBlue: "#00203F",
+	thBlue: "#003a72",
+	thSky: "#3fa1ff",
+	thGreen: "#ADEFD1",
+	thRed: "#f31260",
+	thWhite: "#F4F7FB"
+};
