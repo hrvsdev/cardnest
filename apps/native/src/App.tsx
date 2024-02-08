56469e5df7ee27208e086019e84971654babbeaf
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-08 18:46:56 +0530

    NATIVE | Add init `TabBar`

--- apps/native/src/App.tsx
+++ apps/native/src/App.tsx
@@ -3,13 +3,14 @@ import { StyleSheet, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 
+import { CreditCardIcon, HomeIcon, UserCircleIcon } from "react-native-heroicons/solid";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { CardPreview } from "@components/Card/Preview";
 import { HeaderSearch } from "@components/Header/HeaderSearch.tsx";
 import { HeaderTitle } from "@components/Header/HeaderTitle.tsx";
 
-import { themeColors } from "@styles/colors.ts";
+import { opacity, themeColors } from "@styles/colors.ts";
 
 import { CardFullProfile } from "@libs/types/src/card.ts";
 
@@ -32,9 +33,45 @@ export default function App() {
 			<View style={styles.pageWrapper}>
 				<CardPreview card={CARD} />
 			</View>
+			<View style={styles.tabBar}>
+				<View style={styles.tabButtonWrapper}>
+					<HomeIcon color={themeColors.sky} size={24} />
+				</View>
+				<View style={styles.tabButtonWrapper}>
+					<CreditCardIcon color={themeColors.white.DEFAULT} size={24} />
+				</View>
+				<View style={styles.tabButtonWrapper}>
+					<UserCircleIcon color={themeColors.white.DEFAULT} size={24} />
+				</View>
+			</View>
 		</AppContainer>
 	);
 }
+
+const styles = StyleSheet.create({
+	pageWrapper: {
+		flex: 1,
+		flexDirection: "column",
+		rowGap: 16,
+		padding: 16
+	},
+	tabBar: {
+		position: "absolute",
+		width: "100%",
+		bottom: 0,
+		display: "flex",
+		flexDirection: "row",
+		borderTopWidth: 1,
+		borderTopColor: themeColors.white[10],
+		backgroundColor: opacity(themeColors.black, 0.8)
+	},
+	tabButtonWrapper: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		paddingVertical: 16
+	}
+});
 
 function AppContainer({ children }: PropsWithChildren) {
 	const gradientColors = [themeColors.black, themeColors.darkerBlue];
@@ -49,12 +86,3 @@ function AppContainer({ children }: PropsWithChildren) {
 		</LinearGradient>
 	);
 }
-
-const styles = StyleSheet.create({
-	pageWrapper: {
-		flex: 1,
-		flexDirection: "column",
-		rowGap: 16,
-		padding: 16
-	}
-});
