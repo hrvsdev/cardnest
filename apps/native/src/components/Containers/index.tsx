d79a242cc1f7e734c56c9fe0ea9824bb132358c3
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-09 21:58:26 +0530

    NATIVE | Add `PageRoot` component

--- apps/native/src/components/Containers/index.tsx
+++ apps/native/src/components/Containers/index.tsx
@@ -1,8 +1,18 @@
 import { PropsWithChildren } from "react";
-import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { ScrollView, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type Props = PropsWithChildren<{ style?: StyleProp<ViewStyle> }>;
 
+export function PageRoot({ children, style }: Props) {
+	const insets = useSafeAreaInsets();
+	const styles = StyleSheet.compose({ flex: 1, paddingTop: insets.top }, style);
+
+	return <ScrollView style={styles}>{children}</ScrollView>;
+}
+
 export function PageContainer({ children, style }: Props) {
-	return <View style={StyleSheet.compose({ padding: 16 }, style)}>{children}</View>;
+	const styles = StyleSheet.compose({ padding: 16 }, style);
+	return <View style={styles}>{children}</View>;
 }
