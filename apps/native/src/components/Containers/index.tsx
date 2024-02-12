33fbbd0138a74bc776fee66232aac43648cca8af
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-12 13:44:41 +0530

    NATIVE | Add `BgGradient` to `PageRoot`

--- apps/native/src/components/Containers/index.tsx
+++ apps/native/src/components/Containers/index.tsx
@@ -3,13 +3,19 @@ import { ScrollView, StyleProp, StyleSheet, View, ViewStyle } from "react-native
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import { BgGradient } from "@components/Gradient";
+
 type Props = PropsWithChildren<{ style?: StyleProp<ViewStyle> }>;
 
 export function PageRoot({ children, style }: Props) {
 	const insets = useSafeAreaInsets();
 	const styles = StyleSheet.compose({ flex: 1, paddingTop: insets.top }, style);
 
-	return <ScrollView style={styles}>{children}</ScrollView>;
+	return (
+		<BgGradient>
+			<ScrollView style={styles}>{children}</ScrollView>
+		</BgGradient>
+	);
 }
 
 export function PageContainer({ children, style }: Props) {
