99880921b9bace98c1917332aa2580af6211873e
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-14 12:39:23 +0530

    NATIVE | Add `CopyButton` component

---
+++ apps/native/src/components/Button/CopyButton.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react";
+import { Pressable } from "react-native";
+import * as Clipboard from "expo-clipboard";
+
+import { IconCheck, IconCopy } from "tabler-icons-react-native";
+
+import { themeColors } from "@styles/colors.ts";
+
+type Props = {
+	text: string;
+};
+
+export function CopyButton({ text }: Props) {
+	const [isCopied, setIsCopied] = useState(false);
+
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+	const onClick = async () => {
+		await Clipboard.setStringAsync(text);
+		setIsCopied(true);
+		timeoutRef.current = setTimeout(() => setIsCopied(false), 2000);
+	};
+
+	useEffect(() => {
+		if (timeoutRef.current) clearTimeout(timeoutRef.current);
+	}, []);
+
+	const Copy = <IconCopy size={22} stroke={1.5} color={themeColors.white.DEFAULT} />;
+	const Check = <IconCheck size={22} color={themeColors.white.DEFAULT} />;
+
+	return (
+		<Pressable
+			style={({ pressed }) => ({
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+				transform: [{ translateY: pressed ? 1 : 0 }]
+			})}
+			onPress={onClick}
+		>
+			{isCopied ? Check : Copy}
+		</Pressable>
+	);
+}
