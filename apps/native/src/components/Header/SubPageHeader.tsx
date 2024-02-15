de3a60701dd8327efbecf711c8728801b1ed7e6f
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-15 10:33:03 +0530

    NATIVE | Add right button props to `SubPageHeader`

--- apps/native/src/components/Header/SubPageHeader.tsx
+++ apps/native/src/components/Header/SubPageHeader.tsx
@@ -3,28 +3,49 @@ import { BlurView } from "expo-blur";
 import { router } from "expo-router";
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { IconChevronLeft } from "tabler-icons-react-native";
+import { IconChevronLeft, TablerIcon } from "tabler-icons-react-native";
+
+import { Show } from "@components/Show";
 
 import { themeColors } from "@styles/colors.ts";
 
 type Props = {
 	title: string;
+
+	leftIconLabel?: string;
+
+	rightButtonLabel?: string;
+	rightButtonIcon?: TablerIcon;
+	onRightButtonPress?: () => void;
 };
 
-export function SubPageHeader({ title }: Props) {
+export function SubPageHeader(props: Props) {
 	const insets = useSafeAreaInsets();
 
 	const onBackPress = () => router.back();
 
+	const RightIcon = props.rightButtonIcon ?? (() => null);
+
 	return (
 		// @ts-ignore
-		<BlurView style={{ paddingTop: insets.top }} intensity={0} >
+		<BlurView style={{ paddingTop: insets.top }} intensity={0}>
 			<View style={styles.wrapper}>
-				<Pressable style={styles.back} onPress={onBackPress}>
+				<Pressable style={[styles.sideButtonWrapper, { left: 0 }]} onPress={onBackPress}>
 					<IconChevronLeft strokeWidth={2.5} size={20} color={themeColors.sky} />
-					<Text style={styles.backLabel}>Back</Text>
+					<Text style={styles.sideButtonLabel}>{props.leftIconLabel ?? "Back"}</Text>
 				</Pressable>
-				<Text style={styles.title}>{title}</Text>
+
+				<Text style={styles.title}>{props.title}</Text>
+
+				<Pressable
+					style={[styles.sideButtonWrapper, { right: 0 }]}
+					onPress={props.onRightButtonPress}
+				>
+					<RightIcon size={20} color={themeColors.sky} />
+					<Show when={props.rightButtonLabel}>
+						<Text style={styles.sideButtonLabel}>{props.rightButtonLabel}</Text>
+					</Show>
+				</Pressable>
 			</View>
 		</BlurView>
 	);
@@ -37,24 +58,24 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		justifyContent: "center",
 		height: 48,
-		position: "relative",
+		position: "relative"
 	},
 	title: {
 		fontSize: 16,
 		fontWeight: "500",
 		color: themeColors.white.DEFAULT
 	},
-	back: {
+	sideButtonWrapper: {
 		display: "flex",
 		flexDirection: "row",
 		alignItems: "center",
 		height: "100%",
 		position: "absolute",
-		left: 0,
-		paddingHorizontal: 16,
+		paddingLeft: 16,
+		paddingRight: 20,
 		columnGap: 4
 	},
-	backLabel: {
+	sideButtonLabel: {
 		fontSize: 16,
 		color: themeColors.sky
 	}
