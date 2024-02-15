6f87234669faca728fd092e31fed91e2e5000aeb
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-15 11:06:30 +0530

    NATIVE | Refactor `CardView` page to its separate directory

---
+++ apps/native/src/app/(tabs)/home/card/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+
+import { SCREEN_STYLE } from "@styles/common.ts";
+
+export default function CardDetailsLayout() {
+	const screenOptions: NativeStackNavigationOptions = {
+		headerShown: false,
+		contentStyle: SCREEN_STYLE
+	};
+
+	return <Stack screenOptions={screenOptions} />;
+}
