a35329f2b8d9c4eef67de42629f8391c72ba8d23
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-20 18:45:18 +0530

    NATIVE | Fix `opacity` function not working with values less than `0.1`

--- apps/native/src/styles/colors.ts
+++ apps/native/src/styles/colors.ts
@@ -1,5 +1,5 @@
 export const opacity = (c: string, o: number) => {
-	return c + Math.round(255 * o).toString(16);
+	return c + Math.round(255 * o).toString(16).padStart(2, "0");
 };
 
 export const themeColors = {
