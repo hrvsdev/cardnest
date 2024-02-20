6d15cfd1f17ff9141beb603507d1c0d52867fcde
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-20 18:44:23 +0530

    NATIVE | Add init `User` page

--- apps/native/src/app/(tabs)/user/index.tsx
+++ apps/native/src/app/(tabs)/user/index.tsx
@@ -1,11 +1,31 @@
+import { IconMoonStars, IconPasswordFingerprint, IconTrash } from "tabler-icons-react-native";
+
 import { PageContainer, PageRoot } from "@components/Containers";
 import { HeaderTitle } from "@components/Header/HeaderTitle.tsx";
+import { SettingsGroup } from "@components/Settings";
+import { SettingsButton, SettingsLink } from "@components/Settings/Button.tsx";
 
 export default function Page() {
 	return (
 		<PageRoot>
 			<HeaderTitle title="You" />
-			<PageContainer />
+			<PageContainer style={{ gap: 24 }}>
+				<SettingsGroup title="App settings">
+					<SettingsLink Icon={IconPasswordFingerprint} title="Security" href="security" />
+					<SettingsLink Icon={IconMoonStars} title="User Interface" href="interface" />
+				</SettingsGroup>
+				<SettingsGroup title="Danger zone" description={DELETE_DESCRIPTION}>
+					<SettingsButton
+						isDanger
+						Icon={IconTrash}
+						title="Delete all data"
+						onPress={() => console.log("Delete all data")}
+					/>
+				</SettingsGroup>
+			</PageContainer>
 		</PageRoot>
 	);
 }
+
+const DELETE_DESCRIPTION =
+	"Deleting data will forever delete all data from all your devices and there is no way to recover it.";
