3dcdd26a9bb0072e7205a5e0680095fd3557d84a
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-24 10:32:38 +0530

    NATIVE | Add bg animation on press to `Keypad`

--- apps/native/src/components/Pin/Keypad/index.tsx
+++ apps/native/src/components/Pin/Keypad/index.tsx
@@ -1,13 +1,17 @@
 import { ReactNode } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import * as Haptics from "expo-haptics";
 
 import { BackspaceIcon } from "react-native-heroicons/outline";
-import Animated, { useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, {
+	interpolateColor,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming
+} from "react-native-reanimated";
 
 import { PIN_LENGTH } from "@libs/utils/src/auth.ts";
 
-import { themeColors } from "@styles/colors.ts";
+import { opacity, themeColors } from "@styles/colors.ts";
 
 const SPLITTED_KEYPAD_NUMBERS = [
 	[1, 2, 3],
@@ -74,28 +78,39 @@ type KeypadButtonProps = {
 	disabled?: boolean;
 };
 
+const INIT_BG = opacity(themeColors.white.DEFAULT, 0.05);
+const PRESSED_BG = opacity(themeColors.white.DEFAULT, 0.5);
+
 function KeypadButton({ label, onPress, disabled }: KeypadButtonProps) {
 	const scale = useSharedValue(1);
+	const bgProgress = useSharedValue(0);
 
-	const onPressIn = () => (scale.value = withTiming(0.95, { duration: 50 }));
-	const onPressOut = () => (scale.value = withTiming(1, { duration: 50 }));
-
-	const onKeyPress = async () => {
-		await Haptics.selectionAsync();
-		onPress();
+	const onPressIn = () => {
+		scale.value = withTiming(0.95, { duration: 50 });
+		bgProgress.value = withTiming(1, { duration: 50 });
 	};
 
-	const style = [styles.keypadButton, { transform: [{ scale }], opacity: disabled ? 0.5 : 1 }];
+	const onPressOut = () => {
+		scale.value = withTiming(1, { duration: 50 });
+		bgProgress.value = withTiming(0, { duration: 200 });
+	};
+
+	const animatedStyle = useAnimatedStyle(() => {
+		return {
+			transform: [{ scale: scale.value }],
+			backgroundColor: interpolateColor(bgProgress.value, [0, 1], [INIT_BG, PRESSED_BG])
+		};
+	});
 
 	return (
 		<View style={{ padding: 10 }}>
 			<Pressable
 				disabled={disabled}
-				onPress={onKeyPress}
+				onPress={onPress}
 				onPressIn={onPressIn}
 				onPressOut={onPressOut}
 			>
-				<Animated.View style={style}>
+				<Animated.View style={[styles.keypadButton, animatedStyle, disabled && { opacity: 0.5 }]}>
 					{typeof label === "number" ? <Text style={styles.keypadButtonText}>{label}</Text> : label}
 				</Animated.View>
 			</Pressable>
