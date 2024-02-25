6450d3bc94e0d59f404a9373df3769a255ca9d88
Harsh Vyas <kumarvyasss@gmail.com>
2024-02-25 22:48:59 +0530

    NATIVE | Add init animation to `CardNetworkSelect`

--- apps/native/src/components/Card/Editor/CardThemeSelect.tsx
+++ apps/native/src/components/Card/Editor/CardThemeSelect.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 import * as Haptics from "expo-haptics";
 import { LinearGradient } from "expo-linear-gradient";
 
+import Animated, {
+	interpolateColor,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming
+} from "react-native-reanimated";
+
 import { cardThemes } from "@libs/utils/src/card.ts";
 
-import { colors, themeColors } from "@styles/colors.ts";
+import { colors, opacity, themeColors } from "@styles/colors.ts";
 
 import { CardTheme } from "@libs/types/src/card";
 
@@ -12,6 +20,18 @@ type Props = {
 	theme: CardTheme;
 	setTheme: (value: CardTheme) => void;
 };
+
+type ItemProps = {
+	theme: CardTheme;
+	selected: boolean;
+	onPress: (theme: CardTheme) => void;
+};
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+
+const DURATION = 200;
+const INIT_BORDER_COLOR = opacity(themeColors.white.DEFAULT, 0);
+const SELECTED_BORDER_COLOR = opacity(themeColors.white.DEFAULT, 0.8);
 
 export function CardThemeSelect({ theme, setTheme }: Props) {
 	const onPress = async (theme: CardTheme) => {
@@ -24,21 +44,45 @@ export function CardThemeSelect({ theme, setTheme }: Props) {
 			<Text style={styles.label}>Card theme</Text>
 			<View style={styles.grid}>
 				{cardThemes.map((t) => (
-					<Pressable
-						key={t}
-						onPress={() => onPress(t)}
-						style={[styles.item, theme === t ? styles.selected : styles.notSelected]}
-					>
-						<LinearGradient
-							colors={[colors[t]["500"], colors[t]["700"]]}
-							start={{ x: 0, y: 0 }}
-							end={{ x: 1, y: 1 }}
-							style={{ flex: 1, borderRadius: theme === t ? 6 : 10 }}
-						/>
-					</Pressable>
+					<Item key={t} theme={t} selected={t === theme} onPress={onPress} />
 				))}
 			</View>
 		</View>
+	);
+}
+
+function Item({ theme, selected, onPress }: ItemProps) {
+	const borderColorProgress = useSharedValue(0);
+	const padding = useSharedValue(0);
+	const borderRadius = useSharedValue(10);
+
+	useEffect(() => {
+		borderColorProgress.value = withTiming(selected ? 1 : 0, { duration: DURATION });
+		padding.value = withTiming(selected ? 8 : 0, { duration: DURATION });
+		borderRadius.value = withTiming(selected ? 6 : 8, { duration: DURATION });
+	}, [selected]);
+
+	const itemAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			borderColor: interpolateColor(
+				borderColorProgress.value,
+				[0, 1],
+				[INIT_BORDER_COLOR, SELECTED_BORDER_COLOR]
+			)
+		};
+	});
+
+	return (
+		<Pressable onPress={() => onPress(theme)}>
+			<Animated.View style={[styles.item, itemAnimatedStyle, { padding }]}>
+				<AnimatedLinearGradient
+					colors={[colors[theme]["500"], colors[theme]["700"]]}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 1, y: 1 }}
+					style={{ flex: 1, borderRadius }}
+				/>
+			</Animated.View>
+		</Pressable>
 	);
 }
 
@@ -64,14 +108,7 @@ const styles = StyleSheet.create({
 	item: {
 		width: gridItemWidth,
 		height: 48,
-		borderRadius: 10
-	},
-	selected: {
+		borderRadius: 10,
 		borderWidth: 1,
-		borderColor: themeColors.white["80"],
-		padding: 8
 	},
-	notSelected: {
-		padding: 0
-	}
 });
