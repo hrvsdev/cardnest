485245a5ece86bdc79c6d92a9a1bbcefa70ff4c6
Harsh Vyas <kumarvyasss@gmail.com>
2024-03-05 15:34:44 +0530

    NATIVE | Remove imports

--- apps/native/src/app/(tabs)/home/card/delete.tsx
+++ apps/native/src/app/(tabs)/home/card/delete.tsx
@@ -1,7 +1,5 @@
 import { View } from "react-native";
 
-import { SubPageRoot } from "@components/Containers";
-
 export default function CardDeleteDialog() {
 	return <View style={{ height: 100, backgroundColor: "red" }} />;
 }
