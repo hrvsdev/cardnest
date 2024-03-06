a7a36a832fb30c7a4b41130f3c6d8648ee2662ae
Harsh Vyas <kumarvyasss@gmail.com>
2024-03-06 10:14:33 +0530

    NATIVE | Use stylesheet instead of inline style

--- apps/native/src/app/(tabs)/home/card/delete.tsx
+++ apps/native/src/app/(tabs)/home/card/delete.tsx
@@ -1,5 +1,12 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function CardDeleteDialog() {
-	return <View style={{ height: 100, backgroundColor: "red" }} />;
+	return <View style={styles.wrapper} />;
 }
+
+const styles = StyleSheet.create({
+	wrapper: {
+		height: 100,
+		backgroundColor: "red"
+	}
+});
