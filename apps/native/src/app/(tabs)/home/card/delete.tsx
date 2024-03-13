15850b1cdaaec4a0bc1d79f84af2e25f43378190
Harsh Vyas <kumarvyasss@gmail.com>
2024-03-13 10:08:42 +0530

    NATIVE | Add bottom sheet style and component

--- apps/native/src/app/(tabs)/home/card/delete.tsx
+++ apps/native/src/app/(tabs)/home/card/delete.tsx
@@ -1,12 +1,40 @@
-import { StyleSheet, View } from "react-native";
+import { Fragment } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BlurView } from "expo-blur";
+
+import { themeColors } from "@styles/colors.ts";
 
 export default function CardDeleteDialog() {
-	return <View style={styles.wrapper} />;
+	return (
+		<Fragment>
+			<Backdrop />
+			<View style={styles.wrapper}>
+				<Text style={{ color: themeColors.white.DEFAULT }}>
+					Are you sure you want to delete this card?
+				</Text>
+			</View>
+		</Fragment>
+	);
+}
+
+function Backdrop() {
+	// @ts-ignore
+	return <BlurView style={styles.backdrop} intensity={10} />;
 }
 
 const styles = StyleSheet.create({
+	backdrop: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%"
+	},
 	wrapper: {
-		height: 100,
-		backgroundColor: "red"
+		position: "absolute",
+		bottom: 0,
+		height: 200,
+		width: "100%",
+		backgroundColor: themeColors.darkerBlue
 	}
 });
