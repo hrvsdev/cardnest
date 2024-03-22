12774fe9fe49018275764f13a3804a6ce86ca4bb
Harsh Vyas <kumarvyasss@gmail.com>
2024-03-22 16:14:47 +0530

    NATIVE | Add `style` prop in `BgGradient`

--- apps/native/src/components/Gradient/index.tsx
+++ apps/native/src/components/Gradient/index.tsx
@@ -1,16 +1,17 @@
 import { PropsWithChildren } from "react";
+import { ViewStyle } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import { themeColors } from "@styles/colors.ts";
 
-export function BgGradient({ children }: PropsWithChildren) {
+export function BgGradient({ children, style }: PropsWithChildren<{ style?: ViewStyle }>) {
 	const gradientColors = [themeColors.black, themeColors.darkerBlue];
 
 	const start = { x: 1, y: 0 };
 	const end = { x: 0, y: 1 };
 
 	return (
-		<LinearGradient style={{ flex: 1 }} colors={gradientColors} start={start} end={end}>
+		<LinearGradient style={[{ flex: 1 }, style]} colors={gradientColors} start={start} end={end}>
 			{children}
 		</LinearGradient>
 	);
