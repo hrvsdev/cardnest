c45a1d0f0f1a767bb302ce8e67c3c23bb36cdf55
Harsh Vyas <kumarvyasss@gmail.com>
2024-04-04 11:35:34 +0530

    NATIVE | Remove mmkv use

--- apps/native/src/app/index.tsx
+++ apps/native/src/app/index.tsx
@@ -1,24 +1,12 @@
-import { useEffect } from "react";
 import { Redirect } from "expo-router";
 
-import { MMKV } from "react-native-mmkv";
-
 import { useHasCreatedPin, useIsAuthenticatedValue } from "@libs/hooks/src/auth";
-
-const storage = new MMKV();
 
 export default function RootPage() {
 	const isAuthenticated = useIsAuthenticatedValue();
 	const hasCreatedPin = useHasCreatedPin();
 
 	const showApp = hasCreatedPin ? isAuthenticated : true;
-
-	useEffect(() => {
-		const countValue = storage.getNumber("count") ?? 0;
-		storage.set("count", countValue + 1);
-
-		console.log("Count: ", countValue);
-	}, []);
 
 	return <Redirect href={showApp ? "/home" : "/pin/enter"} />;
 }
