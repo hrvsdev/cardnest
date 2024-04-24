6cff3ce6a33687780184850c64d233924ede1eb2
Harsh Vyas <kumarvyasss@gmail.com>
2024-04-24 13:30:09 +0530

    NATIVE | Replace colors on `Logos` components

--- apps/native/src/components/Logos/MasterCard.tsx
+++ apps/native/src/components/Logos/MasterCard.tsx
@@ -1,6 +1,6 @@
 import Svg, { Circle, G } from "react-native-svg";
 
-import { themeColors } from "@styles/colors.ts";
+import { TH_WHITE_65, TH_WHITE_85 } from "@styles/colors.ts";
 
 export function MasterCard({ width = 40 }: { width?: number }) {
 	const aspectRatio = 100 / 62.5;
@@ -9,8 +9,8 @@ export function MasterCard({ width = 40 }: { width?: number }) {
 		<Svg style={{ width, aspectRatio, overflow: "visible" }} viewBox="0 0 32 20">
 			<G>
 				<G>
-					<Circle cx="10" cy="10" r="10" fill={themeColors.white["85"]} />
-					<Circle cx="22" cy="10" r="10" fill={themeColors.white["65"]} />
+					<Circle cx="10" cy="10" r="10" fill={TH_WHITE_85} />
+					<Circle cx="22" cy="10" r="10" fill={TH_WHITE_65} />
 				</G>
 			</G>
 		</Svg>
--- apps/native/src/components/Logos/Other.tsx
+++ apps/native/src/components/Logos/Other.tsx
@@ -1,7 +1,7 @@
 import { IconForbid2 } from "tabler-icons-react-native";
 
-import { themeColors } from "@styles/colors.ts";
+import { TH_WHITE_85 } from "@styles/colors.ts";
 
 export function Other({ width = 32 }: { width?: number }) {
-	return <IconForbid2 width={width} height={width} fill={themeColors.white["85"]} />;
+	return <IconForbid2 width={width} height={width} fill={TH_WHITE_85} />;
 }
