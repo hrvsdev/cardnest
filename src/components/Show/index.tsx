0c6c702469c44bdcde09e2ca33a7fcc494b5914b
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-13 16:19:26 +0530

    Add `ShowAnimated`

--- src/components/Show/index.tsx
+++ src/components/Show/index.tsx
@@ -1,5 +1,24 @@
 import { PropsWithChildren } from "react";
 
+import { AnimatePresence, motion } from "framer-motion";
+
 export function Show({ when, children }: PropsWithChildren<{ when: any }>) {
 	return when ? <>{children}</> : null;
+}
+
+export function ShowAnimated({ when, children }: PropsWithChildren<{ when: boolean }>) {
+	const initial = { height: 0, opacity: 0 };
+	const animate = { height: "auto", opacity: 1 };
+
+	return (
+		<AnimatePresence>
+			{when && (
+				<div className="overflow-hidden">
+					<motion.div initial={initial} animate={animate} exit={initial}>
+						{children}
+					</motion.div>
+				</div>
+			)}
+		</AnimatePresence>
+	);
 }
