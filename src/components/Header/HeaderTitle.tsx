b9fba1fac8f5e20455caa9bb8b11a000629bcdfb
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-13 16:21:55 +0530

    Restyle `HeaderTitle`

--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -3,5 +3,5 @@ type TitleProps = {
 };
 
 export function HeaderTitle({ title }: TitleProps) {
-	return <h1 className="font-semibold text-heading pl-0.5 pt-8 mx-4">{title}</h1>;
+	return <h1 className="font-bold text-th-white text-heading pt-8 pl-[1.125rem] pr-4">{title}</h1>;
 }
