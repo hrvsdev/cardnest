9050ba4a37d75581162bcf9a8af28b4fa4cc66e4
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-14 12:43:23 +0530

    Restyle `TabBar`

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -24,7 +24,7 @@ const tabs = [
 
 export function TabBar() {
 	return (
-		<div className="flex justify-between sticky bottom-0 border-t border-th-white/10 bg-th-black/80 backdrop-blur-md">
+		<div className="flex justify-between sticky bottom-0 border-t-[0.5px] border-th-white/10 bg-th-black/80 backdrop-blur-md">
 			{tabs.map((tab) => (
 				<TabButton key={tab.label} icon={tab.icon} label={tab.label} path={tab.path} />
 			))}
@@ -40,13 +40,8 @@ type TabButtonProps = {
 
 function TabButton({ icon: I, path }: TabButtonProps) {
 	return (
-		<NavLink
-			to={path}
-			className={({ isActive }) => c("flex-1", isActive ? "text-th-sky" : "text-th-white/70")}
-		>
-			<div className="flex items-center justify-center py-4 gap-2.5 rounded-2xl mx-auto">
-				<I width={24} className={c("transition-colors duration-300")} />
-			</div>
+		<NavLink to={path} className={({ isActive }) => c("flex-1", isActive ? "text-th-sky" : "text-th-white/70")}>
+			<I width={24} className={c("mx-auto py-4")} />
 		</NavLink>
 	);
 }
