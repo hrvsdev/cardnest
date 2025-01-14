e9e61b54986866bc648efe4098304e84233394ea
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-14 12:21:34 +0530

    Restyle `CardPreview`

--- src/components/Card/Preview/index.tsx
+++ src/components/Card/Preview/index.tsx
@@ -33,43 +33,40 @@ export function CardPreview({ card, focused, usePlaceholders, maskCardNumber }: 
 
 		const defaultStyle = "transition-opacity duration-300 ease-in-out";
 		const inFocusStyle = "opacity-100";
-		const outOfFocusStyle = "opacity-55";
+		const outOfFocusStyle = "opacity-60";
 
 		return `${defaultStyle} ${focused === el ? inFocusStyle : outOfFocusStyle}`;
 	};
 
 	return (
 		<div className={c("w-full aspect-payment-card bg-gradient-to-br rounded-2xl font-card", cl)}>
-			<div className="flex flex-col justify-between w-full h-full p-6">
+			<div className="flex flex-col justify-between w-full h-full p-[1.125rem] pb-[0.875rem]">
 				<div className="flex justify-between">
 					<div className={focusedStyle("cardholder")}>
-						<p className="tracking-widest text-2xs font-light text-th-white/80 uppercase">
-							Cardholder
-						</p>
-						<p className="tracking-wider text-lg">{formattedCard.cardholder}</p>
+						<p className="text-2xs text-th-white/70 tracking-widest">CARDHOLDER</p>
+						<p className="text-lg font-medium text-th-white tracking-wider">{formattedCard.cardholder}</p>
 					</div>
+
 					<Show when={formattedCard.issuer}>
 						<div className={c("text-right", focusedStyle("issuer"))}>
-							<p className="tracking-widest text-2xs font-light text-th-white/80 uppercase">
-								Issuer
-							</p>
-							<p className="tracking-wider text-lg">{formattedCard.issuer}</p>
+							<p className="text-2xs text-th-white/70 tracking-widest">ISSUER</p>
+							<p className="text-lg font-medium text-th-white tracking-wider">{formattedCard.issuer}</p>
 						</div>
 					</Show>
 				</div>
-				<div className={c("tracking-widest text-2xl font-bold", focusedStyle("number"))}>
+
+				<div className={c("text-2xl font-bold text-th-white", focusedStyle("number"))}>
 					{formattedCard.number.split("").map((part, i) => (
 						<span key={i} className={c("inline-block text-center", part.trim() ? "w-4" : "w-2")}>
 							{part}
 						</span>
 					))}
 				</div>
+
 				<div className="flex justify-between items-end">
 					<div className={focusedStyle("expiry")}>
-						<p className="tracking-widest text-2xs font-light text-th-white/80 uppercase">
-							Valid Thru
-						</p>
-						<p className="tracking-wider font-medium">
+						<p className="text-2xs text-th-white/70 tracking-widest">VALID THRU</p>
+						<p className="font-bold leading-7 text-th-white">
 							{formattedCard.expiry.split("").map((part, i) => (
 								<span key={i} className="inline-block text-center w-2.5">
 									{part}
@@ -77,7 +74,8 @@ export function CardPreview({ card, focused, usePlaceholders, maskCardNumber }: 
 							))}
 						</p>
 					</div>
-					<div className="pb-1.25">
+
+					<div className="pb-1.5">
 						<CardNetwork />
 					</div>
 				</div>
