082dd91b69c688511dc0a07b2d473920827045c7
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-28 13:27:03 +0530

    Add utils to encode and decode `EncryptedData`

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -5,3 +5,17 @@ export function encode(data: ArrayBuffer): string {
 export function decode(data: string): ArrayBuffer {
 	return Uint8Array.from(atob(data), (c) => c.charCodeAt(0)).buffer;
 }
+
+export function encodeEncryptedData(data: EncryptedData): EncryptedDataEncoded {
+	return {
+		ciphertext: encode(data.ciphertext),
+		iv: encode(data.iv)
+	};
+}
+
+export function decodeEncryptedData(data: EncryptedDataEncoded): EncryptedData {
+	return {
+		ciphertext: decode(data.ciphertext),
+		iv: decode(data.iv)
+	};
+}
