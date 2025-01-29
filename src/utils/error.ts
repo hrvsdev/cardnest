d6e627e28ecaa7fbd82e07647705b5b47d3e420a
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-29 17:50:55 +0530

    Add error classes and methods

---
+++ src/utils/error.ts
@@ -0,0 +1,38 @@
+export class ExtendedError extends Error {
+	constructor(message?: string, cause?: unknown) {
+		super(message, { cause });
+		this.name = "ExtendedError";
+	}
+}
+
+export class InvalidStateError extends ExtendedError {
+	constructor(message?: string, cause?: unknown) {
+		super(message, cause);
+		this.name = "InvalidStateError";
+	}
+}
+
+export class InvalidArgumentError extends ExtendedError {
+	constructor(message?: string, cause?: unknown) {
+		super(message, cause);
+		this.name = "InvalidArgumentError";
+	}
+}
+
+export function toast(e: unknown) {
+	if (e instanceof Error) {
+		// TODO: Implement toast
+		console.log(`Toast: ${e.message}`);
+	}
+}
+
+export function log(e: unknown) {
+	if (e instanceof Error && process.env.NODE_ENV === "development") {
+		console.error(e);
+	}
+}
+
+export function toastAndLog(e: unknown) {
+	toast(e);
+	log(e);
+}
