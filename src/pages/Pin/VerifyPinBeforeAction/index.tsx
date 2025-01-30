85cc656a13e6cd0c4f4a7f1f1e603a7d267cbd6c
Harsh Vyas <kumarvyasss@gmail.com>
2025-01-30 16:14:42 +0530

    Refactor `VerifyPinBeforeAction`

--- src/pages/Pin/VerifyPinBeforeAction/index.tsx
+++ src/pages/Pin/VerifyPinBeforeAction/index.tsx
@@ -1,67 +1,43 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { usePinState } from "@pages/CreatePin/data.ts";
 
-import { PageContainer } from "@components/Containers";
-import { SubPageHeader } from "@components/Header/SubPageHeader.tsx";
+import { SubPageRoot } from "@components/Containers";
 import { Keypad } from "@components/Pin/Keypad";
 import { PinInput } from "@components/Pin/PinInput";
 import { Show } from "@components/Show";
+import { Spacer } from "@components/Spacer";
 
-import { useAfterPinVerified, useSetAfterPinVerified } from "@hooks/actions";
-import { useVerifyPin } from "@hooks/auth";
-
-import { PIN_LENGTH } from "@utils/auth.ts";
+import { afterPinVerified } from "@data/actions";
+import { verifyPin } from "@data/auth";
 
 export function VerifyPinBeforeAction() {
-	const [pin, setPin] = useState<number[]>([]);
-	const [isPinIncorrect, setIsPinIncorrect] = useState(false);
+	const state = usePinState();
 
-	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-	const verifyPin = useVerifyPin();
-	const afterPinVerified = useAfterPinVerified();
-	const setAfterPinVerified = useSetAfterPinVerified();
-
-	const checkPin = async (pinValue: string) => {
-		if (pinValue.length !== PIN_LENGTH) return;
-
-		const isPinCorrect = await verifyPin(pinValue);
-
-		if (!isPinCorrect) {
-			setIsPinIncorrect(true);
-			timeoutRef.current = setTimeout(() => {
-				setPin([]);
-				setIsPinIncorrect(false);
-			}, 1000);
-
-			return;
-		}
-
-		if (afterPinVerified) {
-			afterPinVerified();
-			setAfterPinVerified(null);
-		}
-	};
-
-	useEffect(() => {
-		return () => {
-			if (timeoutRef.current) clearTimeout(timeoutRef.current);
-		};
-	}, []);
+	state.setOnSubmit(async () => {
+		await verifyPin(state.pin);
+		await afterPinVerified.run();
+	});
 
 	return (
-		<Fragment>
-			<SubPageHeader title="" />
-			<PageContainer className="flex flex-col justify-center items-center gap-8">
-				<div className="flex flex-col text-th-white/90 items-center justify-center flex-1">
-					<h1 className="text-2xl font-bold mb-2">Confirm your PIN</h1>
-					<p className="mb-8 px-6 text-center">Please enter your pin to proceed.</p>
-					<PinInput pin={pin} isPinIncorrect={isPinIncorrect} />
-					<p className="mt-6 px-6 text-center text-th-red text-sm h-5">
-						<Show when={isPinIncorrect}>Entered PIN is incorrect</Show>
-					</p>
-				</div>
-				<Keypad pin={pin} setPin={setPin} onPinChange={checkPin} />
-			</PageContainer>
-		</Fragment>
+		<SubPageRoot title="">
+			<Spacer size={32} />
+			<div>
+				<h1 className="text-th-white text-xl font-bold text-center mb-2">Verify your PIN</h1>
+
+				<p className="text-center">Verify your PIN to confirm and proceed.</p>
+			</div>
+
+			<Spacer size={32} />
+			<div className="flex flex-col items-center w-full">
+				<PinInput pin={state.pin} hasError={state.hasError} />
+				<p className="mt-6 text-th-red text-sm">
+					<Show when={state.showErrorMessage}>Entered PIN is incorrect</Show>
+				</p>
+			</div>
+
+			<Spacer className="grow" />
+			<Keypad onKeyClick={state.onKeyClick} onBackspaceClick={state.onBackspaceClick} isDisabled={state.hasMaxLength} />
+
+			<Spacer size={48} />
+		</SubPageRoot>
 	);
 }
