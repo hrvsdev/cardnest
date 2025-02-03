93b9cae55615f1efb0a51f7fc9eb0f7b633b5035
Harsh Vyas <kumarvyasss@gmail.com>
2025-02-03 12:20:45 +0530

    Add `useVerifyPasswordState` hook to manage password verification states

---
+++ src/pages/Password/verify_password_state.ts
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+
+import { toastAndLog } from "@utils/error.ts";
+
+export function useVerifyPasswordState() {
+	const currentPasswordRef = useRef<HTMLInputElement>(null);
+
+	const [currentPasswordValue, setCurrentPasswordValue] = useState("");
+
+	const [isVerifying, setIsVerifying] = useState(false);
+	const [isCurrentPasswordIncorrect, setIsCurrentPasswordIncorrect] = useState(false);
+
+	const onPinSubmit = (afterValidation: () => Promise<void>) => {
+		if (currentPasswordValue.length < 12 || isCurrentPasswordIncorrect) {
+			onCurrentPasswordError();
+			return;
+		}
+
+		setIsVerifying(true);
+		afterValidation().catch((e) => {
+			toastAndLog(e);
+			onCurrentPasswordError();
+		});
+	};
+
+	const onCurrentPasswordError = () => {
+		currentPasswordRef.current?.focus();
+		setIsCurrentPasswordIncorrect(true);
+		setIsVerifying(false);
+	};
+
+	useEffect(() => {
+		if (isCurrentPasswordIncorrect) setIsCurrentPasswordIncorrect(false);
+	}, [currentPasswordValue]);
+
+	useEffect(() => {
+		if (isCurrentPasswordIncorrect) currentPasswordRef.current?.focus();
+	}, [isCurrentPasswordIncorrect]);
+
+	return {
+		currentPasswordValue,
+		setCurrentPasswordValue,
+		currentPasswordRef,
+		isVerifying,
+		isCurrentPasswordIncorrect,
+		onPinSubmit
+	};
+}
