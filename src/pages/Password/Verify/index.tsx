178264ab29861b76ffd12b8093ec8d7508d82abd
Harsh Vyas <kumarvyasss@gmail.com>
2025-02-04 13:32:55 +0530

    Add `VerifyPassword` screen to verify password before action

---
+++ src/pages/Password/Verify/index.tsx
@@ -0,0 +1,52 @@
+import { FormEvent } from "react";
+
+import { useVerifyPasswordState } from "@pages/Password/verify_password_state.ts";
+
+import { Button } from "@components/Button";
+import { SubPageRoot } from "@components/Containers";
+import { PasswordInput } from "@components/Input/PasswordInput.tsx";
+import { PasswordInfo } from "@components/Password/PasswordInfo.tsx";
+import { Spacer } from "@components/Spacer";
+
+import { afterPasswordVerified } from "@data/actions";
+import { verifyPassword } from "@data/auth";
+
+export function VerifyPassword() {
+	const state = useVerifyPasswordState();
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		state.onPinSubmit(async () => {
+			await verifyPassword(state.currentPasswordValue);
+			await afterPasswordVerified.run();
+		});
+	};
+
+	return (
+		<SubPageRoot title="">
+			<Spacer size={32} />
+			<div>
+				<h1 className="text-th-white text-xl font-bold text-center mb-2">Verify your password</h1>
+
+				<p className="text-center">Verify your password to confirm and proceed</p>
+			</div>
+
+			<Spacer size={32} />
+			<form className="flex flex-col grow" onSubmit={onSubmit}>
+				<PasswordInput
+					ref={state.currentPasswordRef}
+					value={state.currentPasswordValue}
+					placeholder="Enter password"
+					isLoading={state.isVerifying}
+					onChange={(e) => state.setCurrentPasswordValue(e.target.value)}
+				/>
+
+				<Spacer size={8} />
+				<PasswordInfo text="Entered password is incorrect" type="ERROR" isVisible={state.isCurrentPasswordIncorrect} />
+
+				<Spacer className="grow" />
+				<Button title="Continue" type="submit" isLoading={state.isVerifying} />
+			</form>
+		</SubPageRoot>
+	);
+}
