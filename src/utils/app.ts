f2cd765403987952ec1ed13a45f8318a12c3fd94
Harsh Vyas <kumarvyasss@gmail.com>
2025-03-02 09:55:07 +0530

    Update version to `1.0.2`

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,6 +1,6 @@
 const MAJOR = 1;
 const MINOR = 0;
-const PATCH = 1;
+const PATCH = 2;
 
 export const VERSION_CODE = MAJOR * 10000 + MINOR * 100 + PATCH;
 export const VERSION_NAME = `${MAJOR}.${MINOR}.${PATCH}`;
